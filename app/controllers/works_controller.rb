f4af920fa056383feea0252d6e6ca33ef6ae7334
lemonjai <leocheung_1986@Hotmail.com>
2017-07-11 19:42:34 -0400

    update the work controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,48 @@
 class WorksController < ApplicationController
+
+  before_action :find_work, only: [:show, :edit, :update, :destroy]
+
   def index
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new work_params
+    if @work.save
+      redirect_to @work, notice: "Work experience has been saved!!!"
+    else
+      render 'new', notice: "Work experience has not been saved!!!"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @work.update work_params
+      redirect_to @work, notice: "Work experience has been updated!!!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @work.destroy
+    redirect_to works_path
+  end
+
+  private
+    def work_params
+      params.require(:work).permit(:title, :content, :link)
+    end
+
+    def find_work
+      @work = Work.find(params[:id])
+    end
 end
