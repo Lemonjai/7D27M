1f1d7fe9b6ec702960987004dd61abc44c46266d
lemonjai <leocheung_1986@Hotmail.com>
2017-07-11 19:54:29 -0400

    finish the work section

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,6 +3,7 @@ class WorksController < ApplicationController
   before_action :find_work, only: [:show, :edit, :update, :destroy]
 
   def index
+    @works = Work.all.order("created_at DESC")
   end
 
   def new
